59dfd10ea2a6fbb14937bc2ccec2d50dd791dd21
Spriithy <theo.dano@gmail.com>
2021-06-06 08:59:37 +0200

    Proper DES/DESinv rounds

--- 3des/DES.cpp
+++ 3des/DES.cpp
@@ -5,13 +5,27 @@
 #include "DES.h"
 
 SequenceD<64> DES::operator()(SequenceD<64> seq) {
-    auto left = seq.right();
-    auto right = m_f(seq.right()) * seq.left();
+    auto left = seq.left();
+    auto right = seq.right();
+
+    for (auto i = 0; i < 16; ++i) {
+        auto tmp = right;
+        right = m_f(right) * left;
+        left = tmp;
+    }
+
     return SequenceD<64>(left, right);
 }
 
 SequenceD<64> DESinv::operator()(SequenceD<64> seq) {
-    auto left = seq.right();
-    auto right = m_finv(seq.right()) * seq.left();
+    auto left = seq.left();
+    auto right = seq.right();
+
+    for (auto i = 0; i < 16; ++i) {
+        auto tmp = right;
+        right = m_finv(right) * left;
+        left = tmp;
+    }
+
     return SequenceD<64>(left, right);
 }
